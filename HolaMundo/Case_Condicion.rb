0584f8c8f2d30013911fa7410aa327fdc5f4f351
Daniel <hacking61@hotmail.com>
2013-05-27 12:51:55 -0500

    Se sube un Ejemplo de Condición Case1.

---
+++ HolaMundo/Case_Condicion.rb
@@ -0,0 +1,12 @@
+class Case_Condicion
+  
+  a=13
+  
+  par = case
+  #SI LA VARIABLE ES PAR
+  
+  # COMPROBAR QUE EL NUMERO DE LA VARIABLE A ES PAR    
+ # when a % 2 == 0 then true
+  when a % 2!= 0 then false
+  end
+end
